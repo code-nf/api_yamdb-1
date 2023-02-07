76d4a143aeec4c2ff96f9cf40f5ab7ff8efd0542
code-nf <s.sutskiy@yandex.ru>
2023-02-07 19:16:13 +0300

    feature/#9

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -8,6 +8,12 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
+from .permissions import (AuthorOrReadOnlyPermission,
+                          IsAuthenticatedOrReadOnly)
+from .serializers import (CommentSerializer, GenreSerializer,
+                          ReviewSerializer)
+from reviews.models import Review, Title
+from django.shortcuts import get_object_or_404
 
 
 from .serializers import (CategoriesSerializer, GenreSerializer,
@@ -150,3 +156,43 @@ class TitlesViewSet(viewsets.ModelViewSet):
             return TitlesWriteSerializer
 
         return TitlesReadSerializer
+
+
+class ReviewViewSet(viewsets.ModelViewSet):
+    serializer_class = ReviewSerializer
+    permission_classes = [
+        IsAuthenticatedOrReadOnly,
+        AuthorOrReadOnlyPermission
+    ]
+
+    def get_queryset(self):
+        title_id = self.kwargs.get('title_id')
+        title = get_object_or_404(Title, pk=title_id)
+        return title.reviews.all()
+
+    def perform_create(self, serializer):
+        title = get_object_or_404(Title, pk=self.kwargs.get('title_id'))
+        author = self.request.user
+        serializer.save(
+            author=author,
+            title=title
+        )
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [
+        IsAuthenticatedOrReadOnly,
+        AuthorOrReadOnlyPermission
+    ]
+
+    def get_queryset(self):
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        return review.comments.all()
+
+    def perform_create(self, serializer):
+        review = get_object_or_404(Review, pk=self.kwargs.get('review_id'))
+        serializer.save(
+            author=self.request.user,
+            review=review
+        )

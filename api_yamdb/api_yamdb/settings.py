9fc6e3f5a71221ccca5953f79341603098adaa58
Ришат Вергасов <Zvezda-Rishat1991@yandex.ru>
2023-02-10 21:43:38 +0300

    Вторая отправка на ревью

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'key'
+SECRET_KEY = os.getenv('SECRET_KEY", default="SUP3R-S3CR3T-K3Y-F0R-MY-PR0J3CT')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
